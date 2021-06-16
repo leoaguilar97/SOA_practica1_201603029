be6dd75f7b13530aff2f5cb2e45bad6e742249b8
Leo <leoaguilarsanchez97@gmail.com>
2021-06-16 00:19:52 -0600

    Update fabric.py

--- fabric.py
+++ fabric.py
@@ -1,7 +1,14 @@
 from fabric2 import Connection
+import os
 
-ip = '52.91.111.116'
-connect_kwargs = { "key_filename" : ['./key.ssh'] }
+ip = os.environ('DEPLOYMENT_IP') or '52.91.111.116'
+key_path = os.environ('DEPLOYMENT_KEY_PATH') or './key.shh'
+
+print('*** INICIANDO DEPLOYMENT ***')
+print('* Conectando a la IP: ' + ip)
+print('* Path a la llave SSH: ' + key_path)
+
+connect_kwargs = { "key_filename" : [key_path] }
 c = Connection(host= ip, user= "leoag", connect_kwargs= connect_kwargs)
 
 def sh(command):
